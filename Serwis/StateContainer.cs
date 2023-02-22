99027f0c4a3dbb3b8a6ed4d623d3dd40d369667a
Łukasz Mastalerz <lakasabasz@gmail.com>
2023-02-22 16:49:50 +0100

    Dodanie localStorage

---
+++ Serwis/StateContainer.cs
@@ -0,0 +1,19 @@
+﻿namespace Serwis;
+
+public class StateContainer
+{
+    private int? _storedValue;
+
+    public int StoredValue
+    {
+        get => _storedValue ?? 0;
+        set
+        {
+            _storedValue = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public event EventHandler? PropertyChanged;
+    private void NotifyStateChanged() => PropertyChanged?.Invoke(this, EventArgs.Empty);
+}
