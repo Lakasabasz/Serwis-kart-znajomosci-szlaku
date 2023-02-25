2b42347bf5ce204f777d67d2751d36ba01346bdb
Łukasz Mastalerz <lakasabasz@gmail.com>
2023-02-25 23:27:47 +0100

    Przesiadka z bootstrap na tailwind

---
+++ Serwis/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [
+      './**/*.html',
+      './**/*.razor'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
